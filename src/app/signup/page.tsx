326fb7cfdb8dbae43d2977979e5336fd7a09a20f
Женя Шабельний <ayneg1212@gmail.com>
2025-08-21 15:57:22 +0300

    couple of new page designs and update to main page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,6 +9,7 @@ import ContentBox from '@/components/ui/content-box';
 import Button from '@/components/ui/button';
 import TextInputGroup from '@/components/ui/text-input-group';
 import DateInputGroup from '@/components/ui/date-input-group';
+import Link from 'next/link';
 
 class SignupPage extends React.Component<SignupPageProps, SignupPageState> {
     constructor(props: SignupPageProps) {
@@ -124,7 +125,7 @@ class SignupPage extends React.Component<SignupPageProps, SignupPageState> {
         );
 
         if (response?.success) 
-            this.props.router?.push('/');
+            this.setState({ stage: 4 });
         else 
             this.setState({ error: 'Sign up failed. Please try again.' });
         
@@ -352,7 +353,34 @@ renderContentForStage3 = () => {
         </ContentBox>
     );
 }
-// 
+//
+
+// Stage 4 - Final
+renderContentForStage4 = () =>{
+    return (
+        <ContentBox height='760px' lheight='700px'>
+            <div className='flex-1 p-20 flex flex-col items-center justify-stretch'>
+                <div className="flex-1 w-full max-w-[500px] flex flex-col items-center mb-[30px]">
+                    <Image src='/logo/Logo.png' alt="Logo" width={215} height={60} className='mb-2'/>
+                    <h1 className="font-title-2 text-[length:var(--title-2-font-size)] tracking-[var(--title-2-letter-spacing)] leading-[var(--title-2-line-height)] mb-4 text-center">
+                        Залишився останній крок
+                    </h1>
+                    <p className='pb-4 font-subtitle-3 font-[number:var(--subtitle-3-font-weight)] text-[#e4e4e4] text-[length:var(--subtitle-3-font-size)] text-center tracking-[var(--subtitle-3-letter-spacing)] leading-[var(--subtitle-3-line-height)] [font-style:var(--subtitle-3-font-style)]'>
+                        Ваш обліковий запис успішно створено. для завершення процесу реєстрації перейдіть на вказану електронну пошту та підтвердіть свою реєстрацію.
+                    </p>
+                    <Image src='/SuccessVector1.png' alt='success' width={126} height={126} className=''/>
+
+                </div>
+                <div className="flex-1 w-full max-w-[500px]">
+                    <div className="space-y-10 flex flex-col font-body-2 text-[length:var(--body-2-font-size)] tracking-[var(--body-2-letter-spacing)] leading-[var(--body-2-line-height)]">
+                        <Link href='' className="w-full h-[60px] button-type-2 font-body-1 text-[#fffefe] text-[length:var(--body-1-font-size)] tracking-[var(--body-1-letter-spacing)] leading-[var(--body-1-line-height)]">Головна</Link>
+                    </div>
+                </div>
+            </div>
+        </ContentBox>
+    );
+}
+//
 
     render() {
         if(this.state.stage === 1) {
@@ -363,6 +391,9 @@ renderContentForStage3 = () => {
         }
         else if (this.state.stage === 3) {
             return this.renderContentForStage3();
+        }
+        else if (this.state.stage === 4) {
+            return this.renderContentForStage4();
         }
     }
 }
