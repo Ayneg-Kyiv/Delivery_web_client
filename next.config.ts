3346af51653d71f8df962352725d41cb7d682330
Женя Шабельний <ayneg1212@gmail.com>
2025-10-01 21:01:48 +0300

    path change

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   images: {
-    domains: ['localhost', process.env.NEXT_PUBLIC_API_URL?.replace(/^https?:\/\//, '') || ''],
+    domains: ['localhost', process.env.NEXT_PUBLIC_FILES_URL?.replace(/^https?:\/\//, '') || ''],
   },
 };
 
